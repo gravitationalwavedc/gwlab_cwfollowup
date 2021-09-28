8f706db599e2bc4695dc85bd630f107c14cbc958
TomReichardt <thomas.reichardt@students.mq.edu.au>
2021-09-28 15:54:34 +1000

    Changing the models to be cleaner
    Summary: Condensed the uploaded and viterbi jobs into one, removed the confusing property on the CWFollowupJob model.
    
    Test Plan: None
    
    Reviewers: llakerink
    
    Reviewed By: llakerink
    
    Differential Revision: https://phab.adacs.org.au/D681

--- src/cwfollowup/admin.py
+++ src/cwfollowup/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CWFollowupJob, ViterbiJob, UploadedCWJob, UploadedCWJobCandidate
+from .models import CWFollowupJob, CWJob, CWJobCandidate
 
 
 @admin.register(CWFollowupJob)
@@ -7,8 +7,8 @@ class CWFollowupJobAdmin(admin.ModelAdmin):
     pass
 
 
-class UploadedCWJobCandidateAdmin(admin.TabularInline):
-    model = UploadedCWJobCandidate
+class CWJobCandidateAdmin(admin.TabularInline):
+    model = CWJobCandidate
 
     def has_change_permission(self, request, obj=None):
         return False
@@ -20,11 +20,6 @@ class UploadedCWJobCandidateAdmin(admin.TabularInline):
         return False
 
 
-@admin.register(UploadedCWJob)
-class UploadedCWJobAdmin(admin.ModelAdmin):
-    inlines = [UploadedCWJobCandidateAdmin, ]
-
-
-@admin.register(ViterbiJob)
-class ViterbiJobAdmin(admin.ModelAdmin):
-    pass
+@admin.register(CWJob)
+class CWJobAdmin(admin.ModelAdmin):
+    inlines = [CWJobCandidateAdmin, ]
--- src/cwfollowup/models.py
+++ src/cwfollowup/models.py
@@ -3,76 +3,54 @@ from django.db import models
 from .variables import cwfollowup_parameters
 
 
-# Viterbi job for which to run followups
-class ViterbiJob(models.Model):
+class CWJob(models.Model):
     user_id = models.IntegerField()
-    viterbi_id = models.CharField(max_length=255, blank=False, null=False)
-
-
-# Holds the results of a different continuous wave search code
-class UploadedCWJob(models.Model):
-    user_id = models.IntegerField()
-
     creation_time = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now_add=True)
 
+    is_uploaded = models.BooleanField(default=False)
+    viterbi_id = models.CharField(max_length=255, null=True)
+
+    def save(self, *args, **kwargs):
+        if (self.is_uploaded and self.viterbi_id):
+            raise Exception("You cannot mark this CWJob as uploaded while providing a Viterbi job ID")
+        if (not self.is_uploaded and not self.viterbi_job):
+            raise Exception("You cannot mark this CWJob as not uploaded without providing a Viterbi Job ID")
+        super().save(*args, **kwargs)
+
 
 # Candidates returned from a different continuous wave search
-class UploadedCWJobCandidate(models.Model):
-    job = models.ForeignKey(UploadedCWJob, related_name='parameter', on_delete=models.CASCADE)
+class CWJobCandidate(models.Model):
+    job = models.ForeignKey(CWJob, related_name='candidate', on_delete=models.CASCADE)
     source_dataset = models.CharField(
         max_length=2,
         choices=cwfollowup_parameters.SOURCE_DATASETS,
         default=cwfollowup_parameters.O1[0],
-        null=True,
-        blank=True
     )
-    candidate_frequency = models.FloatField(null=False, blank=False)
-    target_binary = models.BooleanField(default=True, null=False, blank=False)
-    orbit_tp = models.FloatField(null=True, blank=True)
-    asini = models.FloatField(null=True, blank=True)
-    orbit_period = models.FloatField(null=True, blank=True)
+    candidate_frequency = models.FloatField()
+    target_binary = models.BooleanField(default=True)
+    orbit_tp = models.FloatField(null=True)
+    asini = models.FloatField(null=True)
+    orbit_period = models.FloatField(null=True)
 
 
 # Model to hold all the information for a followup job
 # Can either use a Viterbi job or an uploaded job as a ForeignKey
 class CWFollowupJob(models.Model):
     user_id = models.IntegerField()
-    name = models.CharField(max_length=55, blank=False, null=False)
-    description = models.TextField(blank=True, null=True)
+    name = models.CharField(max_length=55, null=False)
+    description = models.TextField(null=True)
     private = models.BooleanField(default=False)
     creation_time = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now_add=True)
 
-    is_uploaded = models.BooleanField(blank=False, default=False)
-    uploaded_job = models.ForeignKey(
-        UploadedCWJob,
+    cw_job = models.ForeignKey(
+        CWJob,
         related_name="followup_job",
         on_delete=models.PROTECT,
         blank=True,
         null=True
     )
-    viterbi_job = models.ForeignKey(
-        ViterbiJob,
-        related_name="followup_job",
-        on_delete=models.PROTECT,
-        blank=True,
-        null=True
-    )
-
-    @property
-    def cw_job(self):
-        if self.is_uploaded:
-            return self.uploaded_job
-        else:
-            return self.viterbi_job
-
-    def save(self, *args, **kwargs):
-        if (self.is_uploaded and not self.uploaded_job):
-            raise Exception("You cannot mark this followup as uploaded without providing an UploadedCWJob")
-        if (not self.is_uploaded and not self.viterbi_job):
-            raise Exception("You cannot mark this followup as not uploaded without providing a ViterbiJob")
-        super().save(*args, **kwargs)
 
 
 # Model to store which followups have been run for each followup job
