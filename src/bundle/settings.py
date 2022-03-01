806c6a8831030ad9eb517519803267ee8c761541
TomReichardt <thomas.reichardt@students.mq.edu.au>
2022-03-01 00:12:28 +0000

    Bundling
    Attempt at adding bundle to repo
    
    Merged by: Lewis Lakerink <llakerink@swin.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/gwlab_cwfollowup/-/merge_requests/10

--- src/bundle/settings.py
+++ src/bundle/settings.py
@@ -1,8 +1,5 @@
 # The directory where jobs are stored
-job_directory = "/fred/oz988/gwcloud/jobs/"
-
-# Format submission script for specified scheduler.
-scheduler = "slurm"
+job_directory = "/fred/oz986/cwfollowup/jobs/"
 
 # Environment scheduler sources during runtime
-scheduler_env = "/fred/oz988/gwcloud/gwcloud_job_client/bundles/unpacked/fbc9f7c0815f1a83b0de36f957351c93797b2049/venv/bin/activate"
+scheduler_env = "/fred/oz986/gwcloud_job_client/bundles/unpacked/bbb09782b80376c58c1909f447ef07b5dab91630/env.sh"
