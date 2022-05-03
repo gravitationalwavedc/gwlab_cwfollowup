12221d865a7e0b445270a8154bcce4369a6fb95d
TomReichardt <thomas.reichardt@students.mq.edu.au>
2022-05-03 01:15:32 +0000

    Fixing obtaining candidates from viterbi
    Because viterbi is being updated to use file download tokens, I had to fix that in CWFollowup too.
    
    Approved-by: Lewis Lakerink <llakerink@swin.edu.au>
    
    Merged by: TomReichardt <thomas.reichardt@students.mq.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/code/gwlab_cwfollowup/-/merge_requests/13

--- src/cwfollowup/tests/test_views.py
+++ src/cwfollowup/tests/test_views.py
@@ -1,7 +1,11 @@
 import json
+import responses
 from unittest import mock
+
+from django.conf import settings
+
 from cwfollowup.views import perform_viterbi_query, get_min_start_time, get_source_dataset, get_viterbi_candidates, \
-    get_viterbi_result_files
+    get_viterbi_result_files, get_candidate_file_data, get_id_from_token
 from cwfollowup.tests.test_utils import VITERBI_FILE_LIST, VITERBI_CANDIDATE_DATA
 from cwfollowup.tests.testcases import CwFollowupTestCase
 
@@ -11,18 +15,38 @@ class TestViterbiSchemaViews(CwFollowupTestCase):
         self.mock_info = mock.Mock()
         self.mock_info.context.headers = None
         self.mock_info.context.method = None
+        self.job_id = 1
 
-    @mock.patch('cwfollowup.views.requests.request')
-    def test_perform_viterbi_query(self, request_mock):
-        request_mock.return_value.content = json.dumps({'data': 'return_data'})
+        self.responses = responses.RequestsMock()
+        self.responses.start()
+
+        self.addCleanup(self.responses.stop)
+        self.addCleanup(self.responses.reset)
+
+    def test_perform_viterbi_query(self):
+        self.responses.add(
+            responses.POST,
+            settings.GWLAB_VITERBI_GRAPHQL_URL,
+            body=json.dumps({'data': 'return_data'}),
+            status=200
+        )
         return_data = perform_viterbi_query(
             query='query',
             variables={'variable': 1},
             headers=[],
             method='POST'
         )
-        request_mock.assert_called()
         self.assertEqual(return_data, 'return_data')
+
+    @mock.patch('cwfollowup.views.perform_viterbi_query')
+    def test_get_id_from_token(self, perform_viterbi_query_mock):
+        test_id = 'test_id'
+        perform_viterbi_query_mock.return_value = {
+            'generateFileDownloadIds': {
+                'result': [test_id]
+            }
+        }
+        self.assertEqual(get_id_from_token(self.mock_info, self.job_id, 'mock-token'), test_id)
 
     @mock.patch('cwfollowup.views.perform_viterbi_query')
     def test_get_viterbi_result_files(self, result_files_mock):
@@ -31,7 +55,7 @@ class TestViterbiSchemaViews(CwFollowupTestCase):
                 'files': VITERBI_FILE_LIST
             }
         }
-        self.assertEqual(get_viterbi_result_files(self.mock_info, 1), VITERBI_FILE_LIST)
+        self.assertEqual(get_viterbi_result_files(self.mock_info, self.job_id), VITERBI_FILE_LIST)
 
     @mock.patch('cwfollowup.views.perform_viterbi_query')
     def test_get_min_start_time(self, perform_viterbi_query_mock):
@@ -43,54 +67,72 @@ class TestViterbiSchemaViews(CwFollowupTestCase):
                 }
             }
         }
-        self.assertEqual(get_min_start_time(self.mock_info, 1), start_time)
+        self.assertEqual(get_min_start_time(self.mock_info, self.job_id), start_time)
 
     @mock.patch('cwfollowup.views.get_min_start_time')
     def test_get_source_dataset(self, min_start_time_mock):
         min_start_time_mock.return_value = 1126051217
-        self.assertEqual(get_source_dataset(self.mock_info, 1), 'o1')
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), 'o1')
         min_start_time_mock.return_value = (1126051217 + 1137254417)/2
-        self.assertEqual(get_source_dataset(self.mock_info, 1), 'o1')
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), 'o1')
         min_start_time_mock.return_value = 1137254417
-        self.assertEqual(get_source_dataset(self.mock_info, 1), 'o1')
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), 'o1')
 
         min_start_time_mock.return_value = 1164556817
-        self.assertEqual(get_source_dataset(self.mock_info, 1), 'o2')
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), 'o2')
         min_start_time_mock.return_value = (1164556817 + 1187733618)/2
-        self.assertEqual(get_source_dataset(self.mock_info, 1), 'o2')
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), 'o2')
         min_start_time_mock.return_value = 1187733618
-        self.assertEqual(get_source_dataset(self.mock_info, 1), 'o2')
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), 'o2')
 
         min_start_time_mock.return_value = 1238166018
-        self.assertEqual(get_source_dataset(self.mock_info, 1), 'o3')
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), 'o3')
         min_start_time_mock.return_value = (1238166018 + 1269388818)/2
-        self.assertEqual(get_source_dataset(self.mock_info, 1), 'o3')
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), 'o3')
         min_start_time_mock.return_value = 1269388818
-        self.assertEqual(get_source_dataset(self.mock_info, 1), 'o3')
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), 'o3')
 
         min_start_time_mock.return_value = 1126051217 - 1
-        self.assertEqual(get_source_dataset(self.mock_info, 1), None)
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), None)
         min_start_time_mock.return_value = 1137254417 + 1
-        self.assertEqual(get_source_dataset(self.mock_info, 1), None)
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), None)
 
         min_start_time_mock.return_value = 1164556817 - 1
-        self.assertEqual(get_source_dataset(self.mock_info, 1), None)
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), None)
         min_start_time_mock.return_value = 1187733618 + 1
-        self.assertEqual(get_source_dataset(self.mock_info, 1), None)
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), None)
 
         min_start_time_mock.return_value = 1238166018 - 1
-        self.assertEqual(get_source_dataset(self.mock_info, 1), None)
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), None)
         min_start_time_mock.return_value = 1269388818 + 1
-        self.assertEqual(get_source_dataset(self.mock_info, 1), None)
+        self.assertEqual(get_source_dataset(self.mock_info, self.job_id), None)
+
+    @mock.patch('cwfollowup.views.get_id_from_token')
+    @mock.patch('cwfollowup.views.get_viterbi_result_files')
+    def test_get_candidate_file_data(self, file_list_mock, candidate_id_mock):
+        file_list_mock.return_value = VITERBI_FILE_LIST
+        candidate_id_mock.return_value = 'mock-download-id'
+        self.responses.add(
+            responses.GET,
+            settings.GWCLOUD_JOB_CONTROLLER_API_URL + '/file/?fileId=mock-download-id',
+            body=VITERBI_CANDIDATE_DATA,
+            status=200
+        )
+
+        candidate_file_data = get_candidate_file_data(self.mock_info, self.job_id)
+        candidate_id_mock.assert_called_once_with(
+            self.mock_info,
+            self.job_id,
+            "/viterbi/results_a0_phase_loglikes_scores.dat-test-token"
+        )
+        self.assertEqual(candidate_file_data, VITERBI_CANDIDATE_DATA)
 
     @mock.patch('cwfollowup.views.get_source_dataset')
-    @mock.patch('cwfollowup.views.requests.get')
-    @mock.patch('cwfollowup.views.get_viterbi_result_files')
-    def test_obtain_viterbi_candidates(self, file_list_mock, candidate_data_mock, source_dataset_mock):
-        file_list_mock.return_value = VITERBI_FILE_LIST
-        candidate_data_mock.return_value.text = VITERBI_CANDIDATE_DATA
+    @mock.patch('cwfollowup.views.get_candidate_file_data')
+    def test_obtain_viterbi_candidates(self, candidate_data_mock, source_dataset_mock):
+        candidate_data_mock.return_value = VITERBI_CANDIDATE_DATA
         source_dataset_mock.return_value = 'o2'
-        candidates = get_viterbi_candidates(self.mock_info, 1)
+        candidates = get_viterbi_candidates(self.mock_info, self.job_id)
         expected = [
             {
                 'orbit_period': 4995.263,
